32091c94405b451776fff66da95d5ef4d38594e4
Shohei Shibata <shohei51@gmail.com>
2018-01-26 06:06:31 +0100

    Created a basic WebSocket server

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-
-app.use(cors());
-
+const http = require('http');
+const WebSocket = require('ws');
 const routes = require('./config/routes.js');
 
 require('dotenv').load();
+
+app.use(cors());
 
 const port = process.env.PORT || 4000;
 
@@ -25,6 +26,24 @@ app.use(bodyParser.json());
 // Load routes
 routes(app);
 
-app.listen(port, function() {
- console.log(`api running on port ${port}`);
-});
+// setup WebSocket
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws, req) {
+  
+    ws.on('message', function incoming(message) {
+      console.log('received: %s', message);
+    });
+  
+    ws.send('something');
+  });
+
+server.listen(port, function() {
+    console.log(`server listening on port ${port}`);
+})
+
+//app.listen(port, function() {
+ //console.log(`api running on port ${port}`);
+//});
