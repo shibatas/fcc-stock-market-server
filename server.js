1e8b458597834187c56323c1267e3b40d8828b91
Shohei Shibata <shohei51@gmail.com>
2018-01-28 06:31:48 +0100

    Websocket returns sent message

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ wss.on('connection', function connection(ws, req) {
     console.log('connection');
     ws.on('message', function incoming(message) {
       console.log('received: %s', message);
-      ws.send('Received: ', message);
+      ws.send(`Received: ${message}`);
     });
   
     ws.send('Successfully connected');
