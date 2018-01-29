add4be407aa5c58b48290a983ce37f8f6a588971
Shohei <shohei51@gmail.com>
2018-01-29 12:34:15 +0000

    https server api routes working, but websockets have issue from Simple Web Socket Client

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
+const proxyServer = require('http-proxy').createProxyServer({secure: false});
 const WebSocket = require('ws');
 const routes = require('./config/routes.js');
 
 require('dotenv').load();
 
-app.use(cors());
+const mainApp = express();
+mainApp.use(cors());
 
-const port = process.env.PORT || 4000;
+const portHttp = process.env.PORT || 4000;
+const portHttps = process.env.PORT_SECURE || 4001;
 
 // db config
 const mongoDB = process.env.MONGODB_URI;
@@ -22,32 +24,48 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // configure body parser for json format
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+mainApp.use(bodyParser.urlencoded({ extended: true }));
+mainApp.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    console.log('is authorized?', req.client.authorized);
-    next();
-})
+// Route all traffic to https server
+mainApp.all('/*', function(req, res) {
+    console.log('redirect to https');
+    //res.send('redirect to https');
+    proxyServer.web(req, res, {target: 'https://0.0.0.0:8081'});
+});
 
-// setup WebSocket
+// HTTP server
+const serverHttp = http.createServer(mainApp).listen(portHttp, function() {
+    console.log(`non-secure server listening on port ${portHttp}`);
+});
+
+// HTTPS server
+const secureApp = express();
+
+secureApp.use(bodyParser.urlencoded({ extended: true }));
+secureApp.use(bodyParser.json());
+
 const sslOptions = {
     key: fs.readFileSync('key.pem', 'utf8'),
     cert: fs.readFileSync('cert.pem', 'utf8')
 }
 
-// HTTPS server
-//const server = https.createServer(sslOptions, app).listen(port, function() {
-//    console.log(`secure server listening on port ${port}`);
-//});
+secureApp.use(function(req, res, next) {
+    console.log('secure route');
+    next();
+})
 
-// HTTP server
-const server = http.createServer(app).listen(port, function() {
-    console.log(`non-secure server listening on port ${port}`);
+const serverHttps = https.createServer(sslOptions, secureApp).listen(portHttps, function() {
+    console.log(`secure server listening on port ${portHttps}`);
 });
 
+//const serverHttps = http.createServer(secureApp).listen(portHttps, function() {
+    //console.log(`secure server listening on port ${portHttps}`);
+//});
+
+
 const wss = new WebSocket.Server({ 
-    server: server
+    server: serverHttps
 });
 
 wss.on('connection', function connection(ws, req) {
@@ -55,21 +73,13 @@ wss.on('connection', function connection(ws, req) {
     ws.on('message', function incoming(message) {
       console.log('received: %s', message);
       ws.send(`Received: ${message}`);
-      /*
-      wss.clients.forEach(function (client) {
-          console.log('client', client.readyState);
-          if (client.readyState) {
-              ws.send('New data');
-          }
-      });   
-      */
     });
   
     ws.send('Successfully connected');
 });
 
 // Load routes
-routes(app, wss);
+routes(secureApp, wss);
 
 //app.listen(port, function() {
 // console.log(`api running on port ${port}`);
